f02d2e7d87d628734818825de587a3dbc7f5aa38
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-09 21:32:41 -0300

    feat: add use effect

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,36 +1,29 @@
-import { useEffect, useState } from "react";
-import {  useNavigate} from 'react-router-dom'
-import { House, LogOut, NotebookPen } from "lucide-react";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { House, LogOut, Vote } from "lucide-react";
 import { UseAuth } from "@/context/auth-context";
 
 
 export function Sidebar() {
-  const [activeTab, setActiveTab] = useState<"home" | "votacoes" | "participantes">("home")
-  const [ newVotationClicked, setNewVotationClicked] = useState(false)
+  const location = useLocation()
+  
   const { Logout } = UseAuth()
-  // const { newVotationClicked } = UseVotation()
 
   const navigate = useNavigate()
 
-  useEffect(() => {
-    if (newVotationClicked) {
-      setActiveTab("participantes")
-    }
-  }, [newVotationClicked])
-
   function HandleClickAdmin() {
-    setActiveTab("home")
     navigate('/admin')
   }
 
   function HandleClickVotation() {
-    setActiveTab("votacoes")
     navigate('/votations')
   }
 
   function HandleLogout() {
     Logout()
   }
+
+  const isHomeActive = location.pathname.startsWith('/admin') || location.pathname === '/' // fica true se estiver na rota /admin ou /
+  const isVotationsActive = location.pathname.startsWith('/votations')
 
   return (
     <article className="w-[297px] h-screen flex flex-col justify-between bg-[#FAFAFA] px-5 py-7">
@@ -39,18 +32,18 @@ export function Sidebar() {
         <div className="flex flex-col gap-4">
           <button
             onClick={HandleClickAdmin}
-            className={`cursor-pointer px-5 py-3 flex gap-2 rounded-lg ${activeTab === "home" ? "bg-[#7C5AED]/20" : ""}`}
+            className={`cursor-pointer px-5 py-3 flex gap-2 rounded-lg ${isHomeActive ? "bg-[#7C5AED]/20" : ""}`}
           >
-            <House className={`text-[#5D7285] ${activeTab === "home" ? "text-[#7C5AED]" : ""}`} />
-            <p className={`text-[#5D7285] font-semibold ${activeTab === "home" ? "text-[#7C5AED]" : ""}`}>Home</p>
+            <House className={`text-[#5D7285] ${isHomeActive ? "text-[#7C5AED]" : ""}`} />
+            <p className={`text-[#5D7285] font-semibold ${isHomeActive ? "text-[#7C5AED]" : ""}`}>Home</p>
           </button>
   
           <button
             onClick={HandleClickVotation}
-            className={`cursor-pointer px-5 py-3 flex gap-2 rounded-lg ${activeTab === "votacoes" ? "bg-[#7C5AED]/20" : ""}`}
+            className={`cursor-pointer px-5 py-3 flex gap-2 rounded-lg ${isVotationsActive ? "bg-[#7C5AED]/20" : ""}`}
           >
-            <House className={`text-[#5D7285] ${activeTab === "votacoes" ? "text-[#7C5AED]" : ""}`} />
-            <p className={`text-[#5D7285] font-semibold ${activeTab === "votacoes" ? "text-[#7C5AED]" : ""}`}>Votações</p>
+            <Vote className={`text-[#5D7285] ${isVotationsActive ? "text-[#7C5AED]" : ""}`} />
+            <p className={`text-[#5D7285] font-semibold ${isVotationsActive ? "text-[#7C5AED]" : ""}`}>Votações</p>
           </button>
 
           {/* <button
@@ -61,17 +54,17 @@ export function Sidebar() {
             <p className={`text-[#5D7285] font-semibold ${activeTab === "votacoes" ? "text-[#7C5AED]" : ""}`}>Votações</p>
           </button> */}
 
-          {newVotationClicked && (
+          {/* {newVotationClicked && (
             <button
-              onClick={() => setActiveTab("participantes")}
-              className={`px-5 py-3 flex gap-2 rounded-lg ${activeTab === "participantes" ? "bg-[#7C5AED]/20" : ""}`}
+              onClick={() => setActiveTab("participants")}
+              className={`px-5 py-3 flex gap-2 rounded-lg ${activeTab === "participants" ? "bg-[#7C5AED]/20" : ""}`}
             >
-              <NotebookPen className={`text-[#5D7285] ${activeTab === "participantes" ? "text-[#7C5AED]" : ""}`} />
-              <p className={`text-[#5D7285] font-semibold ${activeTab === "participantes" ? "text-[#7C5AED]" : ""}`}>
+              <NotebookPen className={`text-[#5D7285] ${activeTab === "participants" ? "text-[#7C5AED]" : ""}`} />
+              <p className={`text-[#5D7285] font-semibold ${activeTab === "participants" ? "text-[#7C5AED]" : ""}`}>
                 Participantes
               </p>
             </button>
-          )}
+          )} */}
         </div>
       </div>
 
