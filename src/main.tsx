d8149a5d11140afc89a8bcdcb9c76ccd10cea3f1
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-09 21:28:02 -0300

    feat: add websocket e user votes provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,20 @@ import { App } from './App.tsx'
 import { ParticipantsProvider } from './context/participants-provider.tsx'
 import { AuthContextProvider } from './context/auth-context.tsx'
 import { VoteProvider } from './context/vote-context.tsx'
+import { WebSocketProvider } from './context/websocket-context.tsx'
+import { UserVotesProvider } from './context/user-votes-context.tsx'
 
 createRoot(document.getElementById('root')!).render(
 <AuthContextProvider>
   <VoteProvider>
     <ParticipantsProvider>
+     <WebSocketProvider>
+      <UserVotesProvider>
       <StrictMode>
         <App />
-      </StrictMode>,
+      </StrictMode>
+      </UserVotesProvider>
+     </WebSocketProvider>
     </ParticipantsProvider>
    </VoteProvider>
 </AuthContextProvider>
