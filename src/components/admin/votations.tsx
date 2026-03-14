a0060d0d04ab5f986dbcc0e308fb2b613bdaee5c
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-14 15:12:42 -0300

    style: add font-bold e text-2xl in title

--- src/components/admin/votations.tsx
+++ src/components/admin/votations.tsx
@@ -9,13 +9,14 @@ export function Votations() {
   useEffect(() => {
     List()
   }, [List])
-
+  
+  console.log(votes)
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex flex-1 flex-col p-10 overflow-auto">
         <div className="flex justify-between">
-          <h1>Minhas votacoes</h1>
+          <h1 className="font-bold text-xl">Minhas votações</h1>
           <button 
             className="cursor-pointer transition-all hover:text-[#7C5AED]"
             onClick={() => Delete()}
@@ -23,20 +24,29 @@ export function Votations() {
             Excluir histórico
           </button>
         </div>
-        { votes.map(vote => (
           <table 
-            key={vote.id}
-            className="w-full border-separate border-transparent border-spacing-y-2 mt-6 px-2 bg-[#FAFAFA]"
+            className="w-full mt-6 px-2"
           >
-            <tbody>
-                <tr>
-                    <td>{vote.title}</td>
-                    <td>{vote.createdAt && DateFormatter(vote.createdAt)}</td>
-                    <td>Quem ganhou</td>
-                </tr>
+          <thead className="w-screen">
+            <tr className="w-full flex justify-between">
+              <th>Nome da votação</th>
+              <th>Data de Realização</th>
+              <th>Vencedor</th>
+            </tr>
+          </thead>
+           { votes.map((vote, index) => (
+             <tbody
+              key={vote.id}
+              className=""
+            >
+              <tr className={`text-[#5D7285] bg-[#CCBFF8] rounded-lg flex justify-between px-2 py-4 ${index > 0 ? 'mt-8' : ''}`}>
+                <td className="font-medium truncate">{vote.title}</td>
+                <td className="font-bold truncate">{vote.createdAt && DateFormatter(vote.createdAt)}</td>
+                <td className="font-medium truncate">{vote.winnerName}</td>
+              </tr>
             </tbody>
+           )) }
           </table>
-        )) }
       </div>
     </div>
   )
